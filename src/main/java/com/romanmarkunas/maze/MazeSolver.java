de00dd86755bc2220b6581a5b53ab37e14f7946f
Roman Markunas <romanmarkunas@gmail.com>
2018-02-07 23:02:12 +0000

    init gradle project

---
+++ src/main/java/com/romanmarkunas/maze/MazeSolver.java
@@ -0,0 +1,11 @@
+package com.romanmarkunas.maze;
+
+public class MazeSolver {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MazeSolver().getGreeting());
+    }
+}
